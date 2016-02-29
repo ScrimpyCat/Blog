e35f2414da23761a398a6c1a138e272a1309c996
Stefan Johnson <ScrimpyCat@gmail.com>
2016-03-01 05:40:38 +1100

    Replace ampersand in links

--- lib/extensions/string.rb
+++ lib/extensions/string.rb
@@ -3,6 +3,6 @@ class String
     REMOVE = /[!%#\?]/
 
     def linkify
-        downcase.gsub(REPLACE, '-').gsub(REMOVE, '').gsub(/-{2,}/, '-').gsub(/-\z/, '')
+        downcase.gsub(/ & /, ' and ').gsub(REPLACE, '-').gsub(REMOVE, '').gsub(/-{2,}/, '-').gsub(/-\z/, '')
     end
 end
