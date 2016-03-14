79ec57960c8cfa992bc2e3ae2c003c0875006629
Stefan Johnson <ScrimpyCat@gmail.com>
2016-03-14 18:00:54 +1100

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Blog::Application.configure do
     # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
     # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-    # config.force_ssl = true
+    config.force_ssl = true
 
     # Set to :debug to see everything in the log.
     config.log_level = :info
@@ -79,7 +79,7 @@ Blog::Application.configure do
     config.log_formatter = ::Logger::Formatter.new
 
     config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-        r301 /.*/, 'http://blog.scrimpycat.io$&', :if => Proc.new { |env|
+        r301 /.*/, 'https://blog.scrimpycat.io$&', :if => Proc.new { |env|
             env['SERVER_NAME'] != 'blog.scrimpycat.io'
         }
     end
